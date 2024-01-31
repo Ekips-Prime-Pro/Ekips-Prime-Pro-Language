e78378409415a70aae3fabd110fbe7da22c3a8d4
Maximilian Gruendinger <maximiliangruendinger@gmail.com>
2024-01-31 21:57:33 +0100

    Add CTkMenuBar and implement basic functionality

--- texteditor/main.py
+++ texteditor/main.py
@@ -2,33 +2,49 @@ import customtkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 from tkinter import messagebox
+from CTkMenuBar import CTkTitleMenu
 import os
 import sys
 import shutil
 
 
-class gui(tk.TK):
+class gui(tk.CTk):
     def __init__(self):
         super().__init__()
         self.title("Text Editor")
+        self.geometry("800x600")
+        self.resizable(False, False)
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.bind("<Control-s>", self.save)
+        # self.iconbitmap("icon.ico")
+        self.main_programm()
+    
+    def on_closing(self):
+        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+            self.destroy()
+            exit(0)    
+            
+    def save(self, event=None):
+        pass
+    
+    def open(self):
+        pass
+    
+    def main_programm(self):
+        self.reset()
+        self.CTkMenubar(self).add_menu("File")
         
-        
+    def reset(self):
+        for widget in self.winfo_children():
+            widget.destroy()
+    
+    
         
 class logik():
     def __init__(self):
-        self.file = None
-        self.filepath = None
-        self.filename = None
-        self.filetype = None
-        self.filecontent = None
-        self.filecontent_old = None
-        
-    def generate_file(self, filepath, filename):
-        self.file = open(os.path.join(filepath, f"{filename}.f"), "w")
-        self.file.close()
-        
-    def open_file(self, filepath, filename):
-        self.file = open(os.path.join(filepath, filename), "r")
-        self.filecontent = self.file.read()
-        self.filecontent_old = self.filecontent
-        self.file.close()
+        pass
+
+
+if __name__ == "__main__":
+    app = gui()
+    app.mainloop()
