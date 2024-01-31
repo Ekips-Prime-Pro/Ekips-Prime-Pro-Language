35c6f4059157c866208f7b68b64052838be43be2
Maximilian Gruendinger <maximiliangruendinger@gmail.com>
2024-01-31 22:20:14 +0100

    Update GUI menu bar in main.py

--- texteditor/main.py
+++ texteditor/main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
-import tkinter.ttk as ttk
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter import messagebox
 import os
 import sys
 import shutil
@@ -17,9 +19,9 @@ class gui(tk.Tk):
         self.main_programm()
     
     def on_closing(self):
-        if ttk.askokcancel("Quit", "Do you want to quit?"):
+        if messagebox.askokcancel("Quit", "Do you want to quit?"):
             self.destroy()
-            exit(0)    
+            exit(0)
             
     def save(self, event=None):
         pass
@@ -29,7 +31,13 @@ class gui(tk.Tk):
     
     def main_programm(self):
         self.reset()
-        
+        menu_bar = tk.Menu(self, bg="white", fg="black")
+        menu_bar.add_command(label="Open", command=self.open)
+        menu_bar.add_command(label="Save", command=self.save)
+        menu_bar.add_command(label="Save as", command=self.save)
+        menu_bar.add_command(label="Exit", command=self.on_closing)
+        menu_bar.add_cascade(label="File", menu=menu_bar)
+        self.config(menu=menu_bar)
         
     def reset(self):
         for widget in self.winfo_children():
