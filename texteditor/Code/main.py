a87a7d7f6dd2c574ea6e6b8c5e281eac8710b076
Maximilian Gruendinger <maximiliangruendinger@gmail.com>
2024-02-14 14:05:57 +0100

    Fix file content display and remove unnecessary code

--- texteditor/Code/main.py
+++ texteditor/Code/main.py
@@ -70,14 +70,20 @@ class gui:
             file = filedialog.askopenfilename(filetypes=[("Text files", "*.txt"), ("Spike Custom System Programming", "*.scsp")])
             self.file_label.config(text=f"File: {file}")
             with open(file, "r") as f:
+                def delete_line(s, line_num):
+                    lines = s.splitlines()
+                    if line_num < len(lines):
+                        del lines[line_num]
+                    return '\n'.join(lines)  
                 Author = f.readline(1)
                 Author = Author.replace("<Author>", "")
                 Author = Author.replace("</Author>", "")
                 self.file_author.config(text=f"Author: {Author}")
                 # TODO:Delete the Author tag from the str and just let the file_content be the content of the str
+                content = f.readlines()
+                content = delete_line(content, 0)
                 self.file_content.delete("1.0", "end")
                 self.file_content.insert("1.0", f.read())
-                self.file_content.delete("1.0", "2.0")
         except:
             messagebox.showerror("Error", "Error while opening file")
     
@@ -89,8 +95,8 @@ class gui:
         self.file_content = tk.Text(self.root)
         self.file_content.place(x=150, y=20, relwidth=1, relheight=1)
         self.menu_top()
-        self.toolbar()
-        
+        self.toolbar()  
+    
     def licence(self):
         messagebox.showinfo("License", f"Spike Custom System Programming License Agreement\nThis License Agreement (the 'Agreement') is entered into by and between Maximilian Gründinger ('Licensor') and the First Lego League Team known as PaRaMeRoS ('Licensee').\n1. License Grant.\nLicensor hereby grants Licensee a non-exclusive, non-transferable license to use and modify the software program known as Spike Custom System Programming (the 'Program') solely for educational and non-commercial purposes. This license is granted exclusively to the members of the First Lego League Team identified as PaRaMeRoS.\n2. Restrictions.\nLicensee shall not, and shall not permit others to:\na. Use the Program for any purpose other than educational and non-commercial activities within the First Lego League Team.\nb. Allow non-members of the First Lego League Team to use or access the Program.\nc. Commercialize or distribute the Program for financial gain.\nd. Remove or alter any copyright, trademark, or other proprietary notices contained in the Program.\n3. Security.\nLicensor makes no warranties regarding the security of the Program. Licensee acknowledges and agrees that any use of the Program is at their own risk. Licensor shall not be responsible for any security bugs or issues that may arise in connection with the Program.\n4. Term and Termination.\nThis Agreement shall remain in effect until terminated by either party. Licensor reserves the right to terminate this Agreement immediately if Licensee breaches any of its terms. Upon termination, Licensee shall cease all use of the Program and destroy all copies in their possession.\n5. Disclaimer of Warranty.\nTHE PROGRAM IS PROVIDED 'AS IS' WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED. LICENSOR DISCLAIMS ALL WARRANTIES, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.\n6. Limitation of Liability.\nIN NO EVENT SHALL LICENSOR BE LIABLE FOR ANY SPECIAL, INCIDENTAL, INDIRECT, OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE THE PROGRAM, EVEN IF LICENSOR HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.\n7. Governing Law.\nThis Agreement shall be governed by and construed in accordance with the laws of Germany, Bavaria, Munic.\n8. Entire Agreement.\nThis Agreement constitutes the entire agreement between the parties and supersedes all prior agreements, whether oral or written, with respect to the Program.\nIN WITNESS WHEREOF, the parties hereto have executed this License Agreement as of the effective date.\nLicensor:\nMaximilian Gründinger\nLicensee:\nPaRaMeRoS\nDate: 1.1.2024")
     
