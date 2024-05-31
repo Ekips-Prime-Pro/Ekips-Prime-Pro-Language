257ff86b2cf49d369822b0dedc0f4379f042a728
Maximilian Gründinger <iron.ai.dev@gmail.com>
2024-05-31 21:16:16 +0200

    Refactor compiler_gui.py: Add compile_llsp3 function for compiling files to llsp3 format

--- Code/compiler_gui.py
+++ Code/compiler_gui.py
@@ -17,7 +17,7 @@ with open("version", "r") as f:
 def compile(file):
     """
     Compiles the given file if it has a valid extension (.scsp).
-    
+
     Parameters:
         file (str): The path of the file to compile.
     """
@@ -33,10 +33,10 @@ def compile(file):
 def get_active_function(line):
     """
     Extracts the active function and its value from a line.
-    
+
     Parameters:
         line (str): The line to extract the function from.
-    
+
     Returns:
         tuple: A tuple containing the function and its value.
     """
@@ -46,11 +46,11 @@ def get_active_function(line):
     variable = variable.replace("}","")
     variable = variable.replace("\n","")
     return function, variable
-    
+
 def write_function(function,file,value=False):
     """
     Writes the specified function and its value to a Python file.
-    
+
     Parameters:
         function (str): The function to write.
         file (str): The name of the file to write to.
@@ -139,7 +139,7 @@ def write_function(function,file,value=False):
 def debug_function(function,value=False):
     """
     Debugs the specified function.
-    
+
     Parameters:
         function (str): The function to debug.
         value (str, optional): The value associated with the function. Defaults to False.
@@ -220,15 +220,15 @@ def debug_function(function,value=False):
 def compile_llsp3(file):
     """
     This function compiles the file to a llsp3 file.
-    
+
     Parameters:
         file (str): The name of the file to compile.
     """
     file_name, file_ending = file.split(".")
-    
+
     with open(file, "r") as f:
         content = f.read()
-        
+
     with open("llsp3.fll", "rb") as f:
         currentline = 0
         content = f.readlines()
@@ -239,15 +239,15 @@ def compile_llsp3(file):
             else:
                 contentout.append(content)
             currentline += 1
-        
+
     with open(f"{file}.llsp3", "wb") as f:
         f.write(contentout)
-        
+
 
 def main_debug(file):
     """
     Main function for debugging the compiled file.
-    
+
     Parameters:
         file (str): The name of the file to debug.
     """
@@ -259,7 +259,7 @@ def main_debug(file):
 def main(file):
     """
     Main function for compiling the file.
-    
+
     Parameters:
         file (str): The name of the file to compile.
     """
@@ -271,11 +271,11 @@ def main(file):
         # Komentare herausfiltern
         function, value = get_active_function(line)
         write_function(function, file, value)
-   
+
 def check_for_format(requestet, value):
     """
     Checks if the value matches the requested format.
-    
+
     Parameters:
         requestet (str): The requested format.
         value (str): The value to check.
@@ -285,7 +285,7 @@ def check_for_format(requestet, value):
             int(value)
         except:
             messagebox.askokcancel(f"Error: The value {value} is not a valid integer.")
-   
+
 class app:
     """
     Main application class.
@@ -300,7 +300,7 @@ class app:
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.root.iconbitmap("icon.ico")
         self.main_frame()
-        self.root.mainloop()  
+        self.root.mainloop()
 
     def main_frame(self):
         """
@@ -320,12 +320,12 @@ class app:
 
     def licence(self):
         messagebox.showinfo("License", f"Ekips System Programming License Agreement\nThis License Agreement (the 'Agreement') is entered into by and between Maximilian Gründinger ('Licensor') and the First Lego League Team known as PaRaMeRoS ('Licensee').\n1. License Grant.\nLicensor hereby grants Licensee a non-exclusive, non-transferable license to use and modify the software program known as Ekips System Programming (the 'Program') solely for educational and non-commercial purposes. This license is granted exclusively to the members of the First Lego League Team identified as PaRaMeRoS.\n2. Restrictions.\nLicensee shall not, and shall not permit others to:\na. Use the Program for any purpose other than educational and non-commercial activities within the First Lego League Team.\nb. Allow non-members of the First Lego League Team to use or access the Program.\nc. Commercialize or distribute the Program for financial gain.\nd. Remove or alter any copyright, trademark, or other proprietary notices contained in the Program.\n3. Security.\nLicensor makes no warranties regarding the security of the Program. Licensee acknowledges and agrees that any use of the Program is at their own risk. Licensor shall not be responsible for any security bugs or issues that may arise in connection with the Program.\n4. Term and Termination.\nThis Agreement shall remain in effect until terminated by either party. Licensor reserves the right to terminate this Agreement immediately if Licensee breaches any of its terms. Upon termination, Licensee shall cease all use of the Program and destroy all copies in their possession.\n5. Disclaimer of Warranty.\nTHE PROGRAM IS PROVIDED 'AS IS' WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED. LICENSOR DISCLAIMS ALL WARRANTIES, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.\n6. Limitation of Liability.\nIN NO EVENT SHALL LICENSOR BE LIABLE FOR ANY SPECIAL, INCIDENTAL, INDIRECT, OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE THE PROGRAM, EVEN IF LICENSOR HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.\n7. Governing Law.\nThis Agreement shall be governed by and construed in accordance with the laws of Germany, Bavaria, Munic.\n8. Entire Agreement.\nThis Agreement constitutes the entire agreement between the parties and supersedes all prior agreements, whether oral or written, with respect to the Program.\nIN WITNESS WHEREOF, the parties hereto have executed this License Agreement as of the effective date.\nLicensor:\nMaximilian Gründinger\nLicensee:\nPaRaMeRoS\nDate: 1.1.2024")
-   
+
     def on_closing(self):
         if messagebox.askokcancel("Quit", "Do you want to quit?"):
             self.app.destroy()
             exit(0)
-            
+
     def select_file(self):
         global file_name
         file = filedialog.askopenfilename(filetypes=[("Ekips System Programming", "*.ssp")])
@@ -334,32 +334,32 @@ class app:
             self.file = file
             compile(file)
             main(file)
-            
+
     def select_file_deb(self):
         file = filedialog.askopenfilename(filetypes=[("Ekips System Programming", "*.ssp")])
         if file:
             self.file = file
             compile(file)
             main_debug(file)
-            
+
     def on_closing(self):
         if messagebox.askokcancel("Quit", "Do you want to quit?"):
             self.root.destroy()
             exit(0)
-            
+
     def credit(self):
         messagebox.showinfo("Credit", "Maximilian Gründinger\nFirst Lego League Team PaRaMeRoS")
-        
+
     def about(self):
         messagebox.showinfo("About", f"Ekips System Programming\nVersion {__version__}\nMaximilian Gründinger\nFirst Lego League Team PaRaMeRoS")
-    
+
     def github(self):
         website.open("https://github.com/Ekips-Prime-Pro/Ekips-Programming-Language")
-        
+
     def help_web(self):
         website.open("https://parameros.net/pro")
-     
-        
-        
+
+
+
 if __name__ == "__main__":
     app()
