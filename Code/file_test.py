24ef1a1264ea4579cc9262c74e1cb0e0047183d0
Maximilian Gruendinger <maximiliangruendinger@gmail.com>
2024-06-05 22:17:27 +0200

    Refactor code: Improve function names and remove unnecessary comments

--- Code/file_test.py
+++ Code/file_test.py
@@ -1,72 +1,123 @@
-import tkinter as tk
 import zipfile
 import json
 import os
+from datetime import datetime
 
-llsp3_manifest = {
-    "type": "python",
-    "appType": "llsp3",
-    "autoDelete": false,
-    "created": "2024-05-31T20:11:04.017Z",
-    "id": "3hf83l3sq3KS",
-    "lastsaved": "2024-05-31T20:28:39.417Z",
-    "size": 1039,
-    "name": "Projekt 2",
-    "slotIndex": 0,
-    "workspaceX": 120,
-    "workspaceY": 120,
-    "zoomLevel": 0.5,
-    "hardware": {
-        "python": {
-            "type": "flipper"
-        }
-    },
-    "state": {
-        "canvasDrawerOpen": true
-    },
-    "extraFiles": []
-}
-llsp3_projectbody = {
-    "main": "# This will be stupid \nprint(\"Welcome to the fucking univers\")"
-}
-llsp3_icon = '<svg width="60" height="60" xmlns="http://www.w3.org/2000/svg">\n    <g fill="none" fill-rule="evenodd">\n        <g fill="#D8D8D8" fill-rule="nonzero">\n            <path d="M34.613 7.325H15.79a3.775 3.775 0 00-3.776 3.776v37.575a3.775 3.775 0 003.776 3.776h28.274a3.775 3.775 0 003.776-3.776V20.714a.8.8 0 00-.231-.561L35.183 7.563a.8.8 0 00-.57-.238zm-.334 1.6l11.96 12.118v27.633a2.175 2.175 0 01-2.176 2.176H15.789a2.175 2.175 0 01-2.176-2.176V11.1c0-1.202.973-2.176 2.176-2.176h18.49z"/>\n            <path d="M35.413 8.214v11.7h11.7v1.6h-13.3v-13.3z"/>\n        </g>\n        <path fill="#0290F5" d="M23.291 27h13.5v2.744h-13.5z"/>\n        <path fill="#D8D8D8" d="M38.428 27h4.32v2.744h-4.32zM17 27h2.7v2.7H17zM17 31.86h2.7v2.744H17zM28.151 31.861h11.34v2.7h-11.34zM17 36.72h2.7v2.7H17zM34.665 36.723h8.1v2.7h-8.1z"/>\n        <path fill="#0290F5" d="M28.168 36.723h4.86v2.7h-4.86z"/>\n    </g>\n</svg>'
+def calculate_file_size(filepath):
+    return os.path.getsize(filepath)
 
+def create_project(directory, project_id, project_name):
+    os.makedirs(directory, exist_ok=True)
 
-# Pfad zur .llsp3-Datei
-llsp3_file_path = 'Projekt2.llsp3'
-extracted_folder = llsp3_file_path + 'projectbody.json'
+    projectbody_data = {
+        "main": "#"
+    }
+    projectbody_path = os.path.join(directory, 'projectbody.json')
+    with open(projectbody_path, 'w') as file:
+        json.dump(projectbody_data, file)
 
-# Schritt 1: Entpacken der .llsp3-Datei
-with zipfile.ZipFile(llsp3_file_path, 'r') as zip_ref:
-    zip_ref.extractall(extracted_folder)
+    icon_svg_content = """
+    <svg width="60" height="60" xmlns="http://www.w3.org/2000/svg">
+        <g fill="none" fill-rule="evenodd">
+            <g fill="#D8D8D8" fill-rule="nonzero">
+                <path d="M34.613 7.325H15.79a3.775 3.775 0 00-3.776 3.776v37.575a3.775 3.775 0 003.776 3.776h28.274a3.775 3.775 0 003.776-3.776V20.714a.8.8 0 00-.231-.561L35.183 7.563a.8.8 0 00-.57-.238zm-.334 1.6l11.96 12.118v27.633a2.175 2.175 0 01-2.176 2.176H15.789a2.175 2.175 0 01-2.176-2.176V11.1c0-1.202.973-2.176 2.176-2.176h18.49z"/>
+                <path d="M35.413 8.214v11.7h11.7v1.6h-13.3v-13.3z"/>
+            </g>
+            <path fill="#0290F5" d="M23.291 27h13.5v2.744h-13.5z"/>
+            <path fill="#D8D8D8" d="M38.428 27h4.32v2.744h-4.32zM17 27h2.7v2.7H17zM17 31.86h2.7v2.744H17zM28.151 31.861h11.34v2.7h-11.34zM17 36.72h2.7v2.7H17zM34.665 36.723h8.1v2.7h-8.1z"/>
+            <path fill="#0290F5" d="M28.168 36.723h4.86v2.7h-4.86z"/>
+        </g>
+    </svg>
+    """
+    icon_svg_path = os.path.join(directory, 'icon.svg')
+    with open(icon_svg_path, 'w') as file:
+        file.write(icon_svg_content)
 
-# Schritt 2: Lesen und Ändern der projectbody.json
-projectbody_json_path = os.path.join(extracted_folder, 'projectbody.json')
+    projectbody_size = calculate_file_size(projectbody_path)
+    icon_svg_size = calculate_file_size(icon_svg_path)
 
-# Lesen der projectbody.json-Datei
-with open(projectbody_json_path, 'r') as file:
-    projectbody_data = json.load(file)
+    total_size = projectbody_size + icon_svg_size
 
-# Ausgabe des aktuellen Inhalts von projectbody.json
-print("Aktueller Inhalt von projectbody.json:")
-print(json.dumps(projectbody_data, indent=4))
+    current_datetime = datetime.utcnow().isoformat() + 'Z'
 
-# Ändern des Inhalts
-projectbody_data['main'] = projectbody_data['main'].replace("Welcome to the fucking univers", "Welcome to the universe")
+    manifest_data = {
+        "type": "python",
+        "appType": "llsp3",
+        "autoDelete": False,
+        "created": current_datetime,
+        "id": project_id,
+        "lastsaved": current_datetime,
+        "size": total_size, 
+        "name": project_name,
+        "slotIndex": 0,
+        "workspaceX": 120,
+        "workspaceY": 120,
+        "zoomLevel": 0.5,
+        "hardware": {
+            "python": {
+                "type": "flipper"
+            }
+        },
+        "state": {
+            "canvasDrawerOpen": True
+        },
+        "extraFiles": []
+    }
+    manifest_path = os.path.join(directory, 'manifest.json')
+    with open(manifest_path, 'w') as file:
+        json.dump(manifest_data, file)
 
-# Speichern der geänderten projectbody.json-Datei
-with open(projectbody_json_path, 'w') as file:
-    json.dump(projectbody_data, file, indent=4)
+    llsp3_file_path = os.path.join(directory, 'Projekt.llsp3')
+    with zipfile.ZipFile(llsp3_file_path, 'w') as zip_ref:
+        for foldername, subfolders, filenames in os.walk(directory):
+            for filename in filenames:
+                file_path = os.path.join(foldername, filename)
+                arcname = os.path.relpath(file_path, directory)
+                zip_ref.write(file_path, arcname)
 
-# Schritt 3: Zurückpacken in das .llsp3-Format
-new_llsp3_file_path = 'Projekt_neu.llsp3'
+    llsp3_file_size = os.path.getsize(llsp3_file_path)
+    print(f"Die Größe der erstellten .llsp3-Datei beträgt: {llsp3_file_size} Bytes")
 
-# Erstellen einer neuen .llsp3-Datei (ZIP-Format)
-with zipfile.ZipFile(new_llsp3_file_path, 'w') as zip_ref:
-    for foldername, subfolders, filenames in os.walk(extracted_folder):
-        for filename in filenames:
-            file_path = os.path.join(foldername, filename)
-            arcname = os.path.relpath(file_path, extracted_folder)
-            zip_ref.write(file_path, arcname)
+    return llsp3_file_path
 
-print(f'Die geänderte .llsp3-Datei wurde unter {new_llsp3_file_path} gespeichert.')
+# Beispielaufruf der Funktion zum Erstellen des Projekts
+create_project('/mnt/data/example_project', '3hf83l3sq3KS', 'Projekt')
+
+def read_and_modify_projectbody(file_path, new_content):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    data['main'] = new_content
+    with open(file_path, 'w') as file:
+        json.dump(data, file)
+
+    # Aktualisieren der manifest.json nach der Bearbeitung
+    directory = os.path.dirname(file_path)
+    projectbody_size = calculate_file_size(file_path)
+    icon_svg_size = calculate_file_size(os.path.join(directory, 'icon.svg'))
+    total_size = projectbody_size + icon_svg_size
+    manifest_path = os.path.join(directory, 'manifest.json')
+    
+    with open(manifest_path, 'r') as file:
+        manifest_data = json.load(file)
+    
+    manifest_data['size'] = total_size
+    manifest_data['lastsaved'] = datetime.utcnow().isoformat() + 'Z'
+    
+    with open(manifest_path, 'w') as file:
+        json.dump(manifest_data, file)
+
+    # Erstellen des ZIP-Archivs erneut nach der Bearbeitung
+    llsp3_file_path = os.path.join(directory, 'Projekt.llsp3')
+    with zipfile.ZipFile(llsp3_file_path, 'w') as zip_ref:
+        for foldername, subfolders, filenames in os.walk(directory):
+            for filename in filenames:
+                file_path = os.path.join(foldername, filename)
+                arcname = os.path.relpath(file_path, directory)
+                zip_ref.write(file_path, arcname)
+
+    # Ausgabe der Dateigröße des Archivs erneut nach der Bearbeitung
+    llsp3_file_size = os.path.getsize(llsp3_file_path)
+    print(f"Die Größe der aktualisierten .llsp3-Datei beträgt: {llsp3_file_size} Bytes")
+
+# Beispielaufruf der Funktion zum Lesen und Bearbeiten
+read_and_modify_projectbody('/mnt/data/example_project/projectbody.json', '# asedsewf\nprint("Hello world")')
